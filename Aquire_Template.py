3f3c3bd0625764d474816fe88ffd3aebedb6acd5
AakashSunil <axs200020@utdallas.edu>
2021-05-10 01:21:31 -0500

    Acquire Template Update

--- Aquire_Template.py
+++ Aquire_Template.py
@@ -11,7 +11,8 @@ from spacy.pipeline import EntityRuler
 
 nlp = spacy.load('en_core_web_sm')
 ruler = EntityRuler(nlp)
-patterns = [{"label": "ACQUIRE", "pattern": "acquired by"}, {"label": "ACQUIRE", "pattern": "acquired"},{"label": "ACQUIRE", "pattern": "acquire"}, {"label": "ACQUIRE", "pattern": "bought"}, {"label": "ACQUIRE", "pattern": "bought  by"},{"label": "ACQUIRE", "pattern": "took over"},{"label": "ACQUIRE", "pattern": "owns"},{"label": "ACQUIRE", "pattern": "owned"},{"label": "ACQUIRE", "pattern": "own"},{"label": "ACQUIRE", "pattern": "purchased"}, {"label": "ACQUIRE", "pattern": "purchased by"},]
+patterns = [{"label": "ACQUIRE", "pattern": "acquired by"}, {"label": "ACQUIRE", "pattern": "acquired"},{"label": "ACQUIRE", "pattern": "acquire"},]
+# {"label": "ACQUIRE", "pattern": "bought"}, {"label": "ACQUIRE", "pattern": "bought  by"},{"label": "ACQUIRE", "pattern": "took over"},{"label": "ACQUIRE", "pattern": "owns"},{"label": "ACQUIRE", "pattern": "owned"},{"label": "ACQUIRE", "pattern": "own"},{"label": "ACQUIRE", "pattern": "purchased"}, {"label": "ACQUIRE", "pattern": "purchased by"},]
 ruler.add_patterns(patterns)
 nlp.add_pipe(ruler)
 # merge_nps = nlp.create_pipe("merge_noun_chunks")
@@ -59,33 +60,36 @@ def extraction(doc,ner_list,dp_list):
                     template = {"Organization 1": "", "Organization 2": "", "Date": ""}
     
     # B was BUY by A for MONEY
-    
-    # for head in doc:
-        # print(head)
-        # for token in head.children:
-        #     if((token.pos_=="NOUN" and token.dep_ == "nsubjpass")):
-        #         print(ner_list)
-        #         input('b')
-        #         template["item"] = token
-        #         for j in token.children:
-        #             if(j.dep_=="nummod"):
-        #                 template["quantity"]=j
-        #     elif ( token.dep_ == "nsubjpass"):
-        #         template["item"] = token
-        #     if ( token.dep_ == "pobj" ):
-        #         template["buyer"] = token
+    for head in doc:
+        # for head_check in head:
             
-        #     for i in doc:
-        #         if(i.ent_type_ == "MONEY"):
-        #             if(head in list(i.ancestors)):
-        #                 template["price"] = i
-        #         if(i.text.lower()=="from" or i.text.lower()=="of" or i.text.lower()=="in"):
-        #             for j in i.children:
-        #                 if(j.pos_=="PROPN" and j.ent_type_!="GPE"):
-        #                     template["source"]=j
-        #     if (len(template["buyer"]) > 0 and len(template["item"]) > 0):
-        #         list_of_templates.append(template)
-        #         template = {"buyer": "", "item": "", "price": "", "quantity": "", "source": ""}
+            # print(head_check.text,head_check.head.text,head_check.tag_,head_check.dep_)
+
+        # print(head.ent_type_)
+        count_date = sum((x == 'DATE') for x in ner_list.values())
+        if(count_date == 1 and head.ent_type_=="DATE"):
+            temp_date = head
+        
+        if(head.ent_type_=="ACQUIRE"):
+            # root = doc.root
+            
+            for token in head.children:
+                if((token.pos_=="NOUN" and token.dep_ == "nsubjpass")):
+                        # print(ner_list)
+                #         input('b')
+                    template["Organization 2"] = token
+                elif ( token.dep_ == "nsubjpass"):
+                    template["Organization 2"] = token
+                if ( token.dep_ == "pobj" ):
+                    template["Organization 1"] = token
+                if(temp_date != None):
+                        template["Date"] = temp_date
+                elif(head.dep_=="pobj" and head.head.text == "in"):
+                        template["Date"] = token
+                if (len(template["Organization 1"]) > 0 and len(template["Organization 2"]) > 0 ):
+                    list_of_templates.append(template)
+                    template = {"Organization 1": "", "Organization 2": "", "Date": ""}
+
     return list_of_templates
 
 def acquire_relation_check(sentence):
