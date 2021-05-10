9b3da851a24ec57f772f3571628e85e2609fdcc8
AakashSunil <axs200020@utdallas.edu>
2021-05-09 20:08:27 -0500

    Acquire Template in progress 1

--- Aquire_Template.py
+++ Aquire_Template.py
@@ -9,69 +9,26 @@ import spacy
 from spacy.pipeline import EntityRuler
 
 nlp = spacy.load('en_core_web_sm')
-
 ruler = EntityRuler(nlp)
-patterns = [{"label": "BUY", "pattern": "purchased"}, {"label": "BUY", "pattern": "purchased by"},
-                {"label": "BUY", "pattern": "acquired by"}, {"label": "BUY", "pattern": "acquired"},
-            {"label": "BUY", "pattern": "acquire"}, {"label": "BUY", "pattern": "bought"}, 
-            {"label": "BUY", "pattern": "bought  by"},{"label": "BUY", "pattern": "took over"},
-           {"label": "BUY", "pattern": "owns"},{"label": "BUY", "pattern": "owned"},
-            {"label": "BUY", "pattern": "own"}]
+patterns = [{"label": "ACQUIRE", "pattern": "purchased"}, {"label": "ACQUIRE", "pattern": "purchased by"},
+                {"label": "ACQUIRE", "pattern": "acquired by"}, {"label": "ACQUIRE", "pattern": "acquired"},
+            {"label": "ACQUIRE", "pattern": "acquire"}, {"label": "ACQUIRE", "pattern": "bought"}, 
+            {"label": "ACQUIRE", "pattern": "bought  by"},{"label": "ACQUIRE", "pattern": "took over"},
+           {"label": "ACQUIRE", "pattern": "owns"},{"label": "ACQUIRE", "pattern": "owned"},
+            {"label": "ACQUIRE", "pattern": "own"}]
 ruler.add_patterns(patterns)
 nlp.add_pipe(ruler)
-merge_nps = nlp.create_pipe("merge_noun_chunks")
-nlp.add_pipe(merge_nps)
+# merge_nps = nlp.create_pipe("merge_noun_chunks")
+# nlp.add_pipe(merge_nps)
 neuralcoref.add_to_pipe(nlp)
 
-
-def merge_entities(document):
-    with document.retokenize() as retokenizer:
-        for entity in document.ents:
-            retokenizer.merge(entity)
-    return document
-
-
-def getAquire(sentences):
-    final_part=[]
-    for sentence_text in sentences:
-        # try:
-            sentence = nlp(sentence_text)
-            sentence=merge_entities(sentence)
-
-            #BUY
-            ans=extraction(sentence)
-            if(ans!=[]):
-                for i in ans:
-                    temp_dict={}
-                    temp_dict["template"]="BUY"
-                    temp_dict["sentences"]=[]
-                    temp_dict["sentences"].append(sentence.text)
-                    temp_dict["arguments"]={}
-                    temp_dict["arguments"]["1"]=i["buyer"].text
-                    temp_dict["arguments"]["2"]=i["item"].text
-                    if(len(i["price"])==0):
-                        temp_dict["arguments"]["3"]=i["price"]
-                    else:
-                        temp_dict["arguments"]["3"]=i["price"].text
-                    if(len(i["quantity"])==0):
-                        temp_dict["arguments"]["4"]=i["quantity"]
-                    else:
-                        temp_dict["arguments"]["4"]=i["quantity"].text
-                    if(len(i["source"])==0):
-                        temp_dict["arguments"]["5"]=i["source"]
-                    else:
-                        temp_dict["arguments"]["5"]=i["source"].text
-                    final_part.append(temp_dict)
-
-    return final_part
-    
 def extraction(doc):
     template = {"buyer": "", "item": "", "price": "", "quantity": "", "source": ""}
     list_of_templates = []
     
     # A BUY B for MONEY
     for head in doc:
-        if(head.ent_type_=="BUY"):
+        if(head.ent_type_=="ACQUIRE"):
             for token in head.children:
                 if (token.dep_ == "nsubj"):
                     template["buyer"] = token
@@ -97,7 +54,7 @@ def extraction(doc):
     # B was BUY by A for MONEY
     
     for head in doc:
-        if(head.ent_type_=="BUY"):
+        if(head.ent_type_=="ACQUIRE"):
             for token in head.children:
                 if((token.pos_=="NOUN" and token.dep_ == "nsubjpass")):
                     template["item"] = token
@@ -123,3 +80,38 @@ def extraction(doc):
     
     return list_of_templates
 
+
+def getAquire(sentences):
+    final_part=[]
+    for sentence_text in sentences:
+        # try:
+            sentence = nlp(sentence_text)
+            # sentence=merge_entities(sentence)
+
+            #ACQUIRE
+            ans=extraction(sentence)
+            if(ans!=[]):
+                for i in ans:
+                    temp_dict={}
+                    temp_dict["template"]="ACQUIRE"
+                    temp_dict["sentences"]=[]
+                    temp_dict["sentences"].append(sentence.text)
+                    temp_dict["arguments"]={}
+                    temp_dict["arguments"]["1"]=i["buyer"].text
+                    temp_dict["arguments"]["2"]=i["item"].text
+                    if(len(i["price"])==0):
+                        temp_dict["arguments"]["3"]=i["price"]
+                    else:
+                        temp_dict["arguments"]["3"]=i["price"].text
+                    if(len(i["quantity"])==0):
+                        temp_dict["arguments"]["4"]=i["quantity"]
+                    else:
+                        temp_dict["arguments"]["4"]=i["quantity"].text
+                    if(len(i["source"])==0):
+                        temp_dict["arguments"]["5"]=i["source"]
+                    else:
+                        temp_dict["arguments"]["5"]=i["source"].text
+                    final_part.append(temp_dict)
+
+    return final_part
+    
